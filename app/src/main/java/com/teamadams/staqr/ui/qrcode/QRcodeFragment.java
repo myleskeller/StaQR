5f65423fdffe7d4faf0a07a506f9908b8d213668
myles keller <myles.keller@gmail.com>
2020-04-21 23:00:17 -0400

    got xzing qr code scanning working, but it's still fullscreen and the back button doesn't leave the view.

--- app/src/main/java/com/teamadams/staqr/ui/qrcode/QRcodeFragment.java
+++ app/src/main/java/com/teamadams/staqr/ui/qrcode/QRcodeFragment.java
@@ -1,33 +1,102 @@
 package com.teamadams.staqr.ui.qrcode;
 
-import android.annotation.SuppressLint;
-import android.os.Build;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.teamadams.staqr.R;
 
+import static com.teamadams.staqr.Dialog.createDialog;
+
+
 public class QRcodeFragment extends Fragment {
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    @SuppressLint("SetJavaScriptEnabled")
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        View qrcodeViewRoot = inflater.inflate(R.layout.fragment_qrcode, container, false);
 
-        //TODO: qrcode implementation stuff here...
+//    TODO: make not-fullscreen
+//    TODO: get rid of stupid decorations
 
-        return qrcodeViewRoot;
+    private String toast;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        displayToast();
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_qrcode, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        scanFromFragment();
+    }
+
+    public void scanFromFragment() {
+        IntentIntegrator.forSupportFragment(this).setOrientationLocked(true);
+        IntentIntegrator.forSupportFragment(this).setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        IntentIntegrator.forSupportFragment(this).initiateScan();
+    }
+
+    private void displayToast() {
+        if (getActivity() != null && toast != null) {
+            Toast.makeText(getActivity(), toast, Toast.LENGTH_LONG).show();
+            toast = null;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                toast = "Cancelled from fragment";
+            } else {
+                toast = "Scanned from fragment: " + result.getContents();
+            }
+
+            // At this point we may or may not have a reference to the activity
+            displayToast();
+        }
+    }
+
+    private void qrCodeCaptureDecision(String code) {
+        createDialog(getActivity(), "QR Code Captured",
+                "Proceed with captured code " + code + '?', false,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) { // OK
+                        submitQRTicket();
+                    }
+                }, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) { // Cancel
+//                        mScannerView.resumeCameraPreview(QRcodeFragment.this);
+                    }
+                });
+    }
+
+    private void submitQRTicket() {
+        //TODO: make the thing
+    }
+
+    private void returnToHome() { //goes to home fragment
+        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+        navController.popBackStack();
     }
 }
